2168625900ecaf0fc72fb3789018304923f7c66d
foo <foo.avoutar7654@gmail.com>
2021-02-03 11:30:50 +0000

    [feat]EventCreateUsecaseを、サービスクラスを作成してAkkaと統合

--- src/main/scala/example/adapter/events/EventService.scala
+++ src/main/scala/example/adapter/events/EventService.scala
@@ -1,16 +1,38 @@
 package example.adapter.events
 
+import akka.actor.typed.ActorRef
+import akka.actor.typed.Behavior
+import akka.actor.typed.scaladsl.Behaviors
+import scala.collection.immutable
 import example.domain.event.EventRepository
 import scala.concurrent.Future
 import example.usecases.event.CreateEventUsecase
 import example.domain.event.EventIdGenerator
 import example.common.IdGenerator
 import example.domain.event.EventId
+import example.usecases.event.CreateEventParameter
+import example.domain.event.Event
+import scala.util.Try
+import scala.concurrent.ExecutionContext
+
+sealed trait EventServiceCommand;
+final case class CreateEvent(
+    param: CreateEventParameter,
+    replyTo: ActorRef[Try[Event]]
+) extends EventServiceCommand
 
 class EventService(
     repository: EventRepository[Future],
     idGenerator: EventIdGenerator
-) extends CreateEventUsecase[Future] {
+)(implicit val executionContext: ExecutionContext)
+    extends CreateEventUsecase[Future] {
+
     def getRepository: EventRepository[Future] = repository
     def getIdGenerator: IdGenerator[EventId] = idGenerator
+
+    def apply(): Behavior[EventServiceCommand] = Behaviors.receiveMessage {
+    case CreateEvent(param, replyTo) =>
+        save(param).map(result => replyTo ! result)
+        Behaviors.same
+    }
 }
