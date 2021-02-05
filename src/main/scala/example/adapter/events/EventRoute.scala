468c2c388ded81224b9d38d4448bf04685e8a152
foo <foo.avoutar7654@gmail.com>
2021-02-05 15:59:23 +0000

    [update]ルーティング処理実装(実験)

--- src/main/scala/example/adapter/events/EventRoute.scala
+++ src/main/scala/example/adapter/events/EventRoute.scala
@@ -1,42 +1,43 @@
 package example.adapter.events
 
-import example.domain.event.EventRepository
-import scala.concurrent.Future
-import akka.actor.typed.ActorSystem
-import scala.concurrent.ExecutionContext
-import akka.actor.typed.ActorRef
 import akka.http.scaladsl.server.Directives._
 import akka.http.scaladsl.model.StatusCodes
 import akka.http.scaladsl.server.Route
-import example.usecases.event.CreateEventParameter
+import akka.actor.typed.ActorRef
+import akka.actor.typed.ActorSystem
 import akka.actor.typed.scaladsl.AskPattern._
 import akka.util.Timeout
 
+import scala.concurrent.Future
+import scala.concurrent.ExecutionContext
+
+import example.domain.event.EventRepository
+import example.usecases.event.CreateEventParameter
+import scala.util.Try
+import example.domain.event.Event
+import scala.util.Success
+
 class EventRoute(service: ActorRef[EventServiceCommand])(
     implicit val system: ActorSystem[_],
-    implicit val executionContext: ExecutionContext
+    implicit val executionContext: ExecutionContext,
+    implicit val timeout: Timeout
 ) {
 
   import akka.http.scaladsl.marshallers.sprayjson.SprayJsonSupport._
-    private implicit val timeout = Timeout.create(system.settings.config.getDuration("my-app.routes.ask-timeout"))
-
   import EventJsonFormats._
 
-  var routes: Route =
+
+  val routes =
     pathPrefix("events") {
-      concat(
-        pathEnd {
-          concat(
-            post {
-              entity(as[CreateEventParameter]) { param =>
-                onSuccess(service.ask(CreateEvent(param, _))) { result =>
-                  result.map(event => complete(StatusCodes.OK, event))
-                    .recover(_ => complete(StatusCodes.InternalServerError))
-                }
-              }
+      pathEnd {
+        post {
+          entity(as[CreateEventParameter]) { param =>
+            onSuccess(service.ask(CreateEvent(param, _))) { result =>
+              result.map(event => complete((StatusCodes.OK, event)))
+                .getOrElse(complete((StatusCodes.InternalServerError)))
             }
-          )
+          }
         }
-      )
+      }
     }
 }
